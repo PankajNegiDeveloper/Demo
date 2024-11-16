4218098990ee34a597931aed861471e127e27854
PankajNegiDeveloper <Pankajsinghnegi158@gmail.com>
2024-11-16 18:23:06 +0530

    Added Controller

---
+++ demo/src/main/java/com/example/demo/MyController.java
@@ -0,0 +1,11 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MyController {
+	
+	public void save() {
+		System.out.println("Heelo Spring Boot");
+	}
+}
